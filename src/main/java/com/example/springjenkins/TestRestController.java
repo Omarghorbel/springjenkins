227c728f3b660ad99f9a9a07920244a33c32b774
omarghorbel <ghromar@gmail.com>
2023-11-25 04:18:24 +0100

    fd

---
+++ src/main/java/com/example/springjenkins/TestRestController.java
@@ -0,0 +1,16 @@
+package com.example.springjenkins;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/omar")
+public class TestRestController {
+
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "hi";
+    }
+}
